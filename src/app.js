bf27e459d507f0c0ee666e4078cb45ba83a55a9d
Henry Osadzinski <henryoz@users.noreply.github.com>
2015-07-20 09:21:09 +0100

    Update watch app display

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 var UI = require('ui');
 var ajax = require('ajax');
 
-var woeid = '13963';
-var URL = 'https://pollencheck.p.mashape.com/api/1/forecasts/' + woeid;
-
 var card = new UI.Card({
-  title: 'Pollen Count',
-  subtitle: 'Testing...'
+  title: "Today's pollen count",
+  subtitle: "Fetching..."
 });
+
+card.show();
+
+// "Where on Earth ID" - currently hard-set to Bristol. 
+// TODO: Pass through via config (using location or manually set).
+var woeid = '13963';
+// The API call, with woeid appended.
+var URL = 'https://pollencheck.p.mashape.com/api/1/forecasts/' + woeid;
 
 ajax(
   {
@@ -18,14 +23,19 @@ ajax(
       'Accept': 'application/json'
     }
   },
+  
   function(data) {
-    console.log(data.periods[0].day);
+//     Grab the arrays for info for the current day, the next day, and two days into the future.
+    var today = data.periods[0];
+//     var tomorrow = data.periods[1];
+//     var twoDays = data.periods[2];
+    card.subtitle(today.combined.avgLevel);
   },
+  
   function(error) {
     console.log('Failed to grab the weather data ' + error);
+    card.title('Oops!');
+    card.subtitle('Something went wrong.');
+    card.body('Sorry! Please try reloading the app.');
   }
 );
-
-
-
-card.show();
